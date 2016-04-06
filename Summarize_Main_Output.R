0a094790dade202959bab8d8c00ea8bbcd5e0a59
Chantel Wetzel <chantell.wetzel@noaa.gov>
2016-04-06 10:04:51 -0700

    Additional Data Summary Work

--- Summarize_Main_Output.R
+++ Summarize_Main_Output.R
@@ -12,19 +12,19 @@
 drive = "C:"
 sim.range = c(1,200)
 
-steep.vec <- c("Steep_85", "Steep_75", "Steep_95", "Steep_85_75", "Steep_85_95")#, "Steep_85_auto")
+steep.vec <- c("Steep_85", "Steep_75", "Steep_95", "Steep_85_75", "Steep_85_95", "Steep_85_auto")
 hcr.vec   <- c( "hcr_25_5_ramp_constant", 
 			    "hcr_40_10_ramp_constant", 
 				"hcr_30_10_ramp_constant", 
-				"hcr_20_05_ramp_constant")
+				"hcr_20_5_ramp_constant")
 
-for (a in 1:length(steep.vec)){
+for (a in 5:length(steep.vec)){
 
 	# Dimensions by Steepness  ===============================================================
-	code.dir = paste("C:/Flatfish_MSC/", sep = "")
-	# code.dir = paste("C:/Users/Chantell.Wetzel/Documents/GitHub/Ch4_Flatfish_HCR/", sep = "")
-	source(paste(code.dir,"code/functions/LH_parameters.R",sep = ""))
-	source(paste(code.dir,"code/functions/Data_Scenarios.R",sep = ""))
+	#code.dir = paste("C:/Flatfish_MSC/code", sep = "")
+	code.dir = paste("C:/Users/Chantell.Wetzel/Documents/GitHub/Chapter4_HCR/", sep = "")
+	source(paste(code.dir,"/functions/LH_parameters.R",sep = ""))
+	source(paste(code.dir,"/functions/Data_Scenarios.R",sep = ""))
 	# The specific years an assessment was performed
 	ass.yrs      = seq(ages + setup.yrs - 1, total.yrs, ass.freq) 
 	# Years to summarize the performance over
@@ -32,6 +32,8 @@ for (a in 1:length(steep.vec)){
 	# Target stock sizes by hcr
 	target = c(0.25, 0.40, 0.30, 0.20)
 	msst.vec = 0.50*target
+	# Quantiles for summary statistics
+	quant.vec = c(0.10, 0.50, 0.90)
 
 	# Create the Storage Matrices and Arrays ================================================================================
 	ssb   <- array(0, dim = c(length(hcr.vec), total.yrs, sim.range[2] - sim.range[1] + 1))
@@ -41,19 +43,22 @@ for (a in 1:length(steep.vec)){
 	
 	ssb.est    <- array(0, dim = c(length(hcr.vec), total.yrs, ass.num, sim.range[2] - sim.range[1] + 1))
 	depl.est   <- array(0, dim = c(length(hcr.vec), total.yrs, ass.num, sim.range[2] - sim.range[1] + 1))
-	rec.est    <- array(0, dim = c(length(hcr.vec), total.yrs, ass.num, sim.range[2] - sim.range[1] + 1))
-	
+	rec.est    <- array(0, dim = c(length(hcr.vec), total.yrs, ass.num, sim.range[2] - sim.range[1] + 1))	
 	acl.est    <- array(0, dim = c(length(hcr.vec), total.yrs, sim.range[2] - sim.range[1] + 1))
 	ofl.est    <- array(0, dim = c(length(hcr.vec), total.yrs, sim.range[2] - sim.range[1] + 1))
 	
+	catch.var  <- array(0, dim = c(length(hcr.vec), length(sum.yrs)))
 	cum.catch  <- array(0, dim = c(length(hcr.vec), sim.range[2] - sim.range[1] + 1))
 	mean.catch <- array(0, dim = c(length(hcr.vec), sim.range[2] - sim.range[1] + 1))
 	catch.sum  <- array(0, dim = c(length(hcr.vec), sim.range[2] - sim.range[1] + 1))
+	catch.ave  <- array(0, dim = c(length(hcr.vec), sim.range[2] - sim.range[1] + 1))
 	aav        <- array(0, dim = c(length(hcr.vec), sim.range[2] - sim.range[1] + 1))
 	below.msst.est  <- array(0, dim = c(length(hcr.vec), project.yrs + 1))
 	below.msst.true <- array(0, dim = c(length(hcr.vec), project.yrs + 1))
-	target.est  <- array(0, dim = c(length(hcr.vec), project.yrs + 1))
-	target.true <- array(0, dim = c(length(hcr.vec), project.yrs + 1))
+	target.est      <- array(0, dim = c(length(hcr.vec), project.yrs + 1))
+	target.true     <- array(0, dim = c(length(hcr.vec), project.yrs + 1))
+	target.20.est   <- array(0, dim = c(length(hcr.vec), project.yrs + 1))
+	target.20.true  <- array(0, dim = c(length(hcr.vec), project.yrs + 1))
 
 	med.ssb.est  <- array(0, dim = c(length(hcr.vec), total.yrs, ass.num, 3))
 	med.depl.est <- array(0, dim = c(length(hcr.vec), total.yrs, ass.num, 3))
@@ -78,21 +83,21 @@ for (a in 1:length(steep.vec)){
 			ssb.est[b, ,1:ass.num, c] <- Est$SB [1:total.yrs, 1:ass.num]
 			depl.est[b,,1:ass.num, c] <- Est$Bratio[1:total.yrs, 1:ass.num]
 			rec.est[b, ,1:ass.num, c] <- Est$Recruits[1:total.yrs, 1:ass.num]
-			acl.est[b,,c]      <- Est$ForeCat[1:total.yrs]
-			ofl.est[b,,c]      <- Est$OFL[1:total.yrs]
+			acl.est[b,,c]             <- Est$ForeCat[1:total.yrs]
+			ofl.est[b,,c]             <- Est$OFL[1:total.yrs]
 		} # simulation loop closure
 
 		# Calculate medians and relative errors====================================================
-		med.ssb[b,,]   = t(apply( ssb[b,,],  1, quantile, c(0.025, 0.50, 0.975), na.rm = T))
-		med.depl[b,,]  = t(apply(depl[b,,],  1, quantile, c(0.025, 0.50, 0.975), na.rm = T))
-		med.rec[b,,]   = t(apply( rec[b,,],  1, quantile, c(0.025, 0.50, 0.975), na.rm = T))
+		med.ssb[b,,]   = t(apply( ssb[b,,],  1, quantile, quant.vec, na.rm = T))
+		med.depl[b,,]  = t(apply(depl[b,,],  1, quantile, quant.vec, na.rm = T))
+		med.rec[b,,]   = t(apply( rec[b,,],  1, quantile, quant.vec, na.rm = T))
 
 		for (d in 1:ass.num){
 			ind = ages:ass.yrs[d]
-			med.ssb.est [b,ind,d,] <- t(apply(ssb.est [b,ind,d,], 1, quantile, c(0.025,0.50,0.975)))
-			med.depl.est[b,ind,d,] <- t(apply(depl.est[b,ind,d,], 1, quantile, c(0.025,0.50,0.975)))
+			med.ssb.est [b,ind,d,] <- t(apply(ssb.est [b,ind,d,], 1, quantile, quant.vec))
+			med.depl.est[b,ind,d,] <- t(apply(depl.est[b,ind,d,], 1, quantile, quant.vec))
 			ind = ages:(ass.yrs[d]- 1)
-			med.rec.est [b,ind,d,] <- t(apply(rec.est [b,ind,d,], 1, quantile, c(0.025,0.50,0.975)))
+			med.rec.est [b,ind,d,] <- t(apply(rec.est [b,ind,d,], 1, quantile, quant.vec))
 		}
 
 		# Calculate performance metrics ==========================================================
@@ -104,8 +109,13 @@ for (a in 1:length(steep.vec)){
 			aav[b,c] = 100*(sum(abs.catch))/sum.catch.by.sim
 
 			# Total catch over the summary period
-		    catch.sum[b,c] = sum.catch.by.sim
+		    catch.sum[b,c] = sum.catch.by.sim		    
 		}	
+
+		# Average catch over simulations
+		catch.ave[b,] = catch.sum[b,]/length(sum.yrs)
+		# Variance of catch across simulations
+		catch.var[b,] = apply(acl.est[b,sum.yrs,], 1, sd)
 
 		# Calculate the percentage the P < MSST
 		for (d in 1:ass.num){
@@ -116,7 +126,7 @@ for (a in 1:length(steep.vec)){
 			}
 
 			if (d != 1) { 
-				ind = (ass.yrs[d-1]+1):ass.yrs[d]
+				ind  = (ass.yrs[d-1]+1):ass.yrs[d]
 				temp = (ass.yrs[d] - ass.yrs[1] - 3):(ass.yrs[d] - ass.yrs[1] + 1) 
 				below.msst.est[b,temp]  = apply(depl.est[b,ind,d,] < msst.vec[b], 1, sum)
 				below.msst.true[b,temp] = apply(depl[b,ind,] < msst.vec[b], 1, sum)
@@ -124,25 +134,43 @@ for (a in 1:length(steep.vec)){
 		}
 		
 		# Calculate the percentage the P < 0.10 +- Target Stock Size
+		N = sim.range[2] - sim.range[1] + 1
 		for (d in 1:ass.num){
 			if (d == 1) { 
 				ind = ass.yrs[1] ; temp = 1 
 
-				target.est[b,temp]  = sum(ifelse(depl.est[b,ind,d,] < target[b] - 0.10*target[b], 0, 
-									      ifelse(depl.est[b,ind,d,] < target[b] + 0.10*target[b], 1, 0)))
-				target.true[b,temp] = sum(ifelse(depl[b,ind,] < target[b] - 0.10*target[b], 0, 
-									      ifelse(depl[b,ind,] < target[b] + 0.10*target[b], 1, 0)))
+				target.est[b,temp]  = sum(ifelse(depl.est[b,ind,d,] < (target[b] - 0.10*target[b]), 0, 
+									      ifelse(depl.est[b,ind,d,] < (target[b] + 0.10*target[b]), 1, 0)))
+
+				target.true[b,temp] = sum(ifelse(depl[b,ind,] < (target[b] - 0.10*target[b]), 0, 
+									      ifelse(depl[b,ind,] < (target[b] + 0.10*target[b]), 1, 0)))
+
+				target.20.est[b,temp]  = sum(ifelse(depl.est[b,ind,d,] < (target[b] - 0.20*target[b]), 0, 
+									         ifelse(depl.est[b,ind,d,] < (target[b] + 0.20*target[b]), 1, 0)))
+
+				target.20.true[b,temp] = sum(ifelse(depl[b,ind,] < (target[b] - 0.20*target[b]), 0, 
+									         ifelse(depl[b,ind,] < (target[b] + 0.20*target[b]), 1, 0)))
 			}
 
 			if (d != 1) { 
-				ind = (ass.yrs[d-1]+1):ass.yrs[d]
+				ind  = (ass.yrs[d-1]+1):ass.yrs[d]
 				temp = (ass.yrs[d] - ass.yrs[1] - 3):(ass.yrs[d] - ass.yrs[1] + 1) 
-				target.est[b,temp]  = apply(ifelse(depl.est[b,ind,d,] < target[b] - 0.10*target[b], 0, 
-									        ifelse(depl.est[b,ind,d,] < target[b] + 0.10*target[b], 1, 0)), 1, sum)
-				target.true[b,temp] = apply(ifelse(depl[b,ind,] < target[b] - 0.10*target[b], 0, 
-									        ifelse(depl[b,ind,] < target[b] + 0.10*target[b], 1, 0)), 1, sum)
+				target.est[b,temp]  = apply(ifelse(depl.est[b,ind,d,] < (target[b] - 0.10*target[b]), 0, 
+									        ifelse(depl.est[b,ind,d,] < (target[b] + 0.10*target[b]), 1, 0)), 1, sum)
+				target.true[b,temp] = apply(ifelse(depl[b,ind,] < (target[b] - 0.10*target[b]), 0, 
+									        ifelse(depl[b,ind,] < (target[b] + 0.10*target[b]), 1, 0)), 1, sum)
+
+				target.20.est[b,temp]  = apply(ifelse(depl.est[b,ind,d,] < (target[b] - 0.20*target[b]), 0, 
+									           ifelse(depl.est[b,ind,d,] < (target[b] + 0.20*target[b]), 1, 0)), 1, sum)
+				target.20.true[b,temp] = apply(ifelse(depl[b,ind,] < (target[b] - 0.20*target[b]), 0, 
+									           ifelse(depl[b,ind,] < (target[b] + 0.20*target[b]), 1, 0)), 1, sum)
 			}
-		}	
+		}
+		target.est[b,]  = target.est[b,] / N
+		target.true[b,] = target.true[b,] / N	
+
+		target.20.est[b,]  = target.20.est[b,] / N
+		target.20.true[b,] = target.20.true[b,] / N	
 
 	} # HCR loop closure
 
@@ -168,10 +196,14 @@ for (a in 1:length(steep.vec)){
 
 	hcr.all$aav            <- aav
 	hcr.all$catch.sum      <- catch.sum
+	hcr.all$catch.ave      <- catch.ave
+	hcr.all$catch.var      <- catch.var
 	hcr.all$below.msst.est <- below.msst.est
 	hcr.all$below.msst.true<- below.msst.true
 	hcr.all$target.est     <- target.est
 	hcr.all$target.true    <- target.true
+	hcr.all$target.20.est  <- target.20.est
+	hcr.all$target.20.true <- target.20.true
 	save(hcr.all, file = hcr.out)
 
 	med.all$med.ssb	    <- med.ssb
